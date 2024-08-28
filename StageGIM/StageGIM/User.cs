930e09086e0345ec11e5f9b9e40a2c0c6daadb13
Ruveydabal <ruveydabal024@gmail.com>
2024-08-28 11:34:27 +0200

    user
    class user start

--- StageGIM/StageGIM/User.cs
+++ StageGIM/StageGIM/User.cs
@@ -2,23 +2,13 @@
 
 namespace ManagementSytem.clases
 {
-    public class User
+    public class User(string name, string userId, string email, string borrowedBooks)
     {
         // Private fields to store the properties of the user
-        private string Name;
-        private string UserId;
-        private string Email;
-        private string BorrowedBooks;
-
-        // Constructor to initialize the fields of the book
-        public User(string name, string userId, string email, string borrowedBooks)
-        {
-            Name = name;
-            UserId = userId;
-            Email = email;
-            BorrowedBooks = borrowedBooks;
-        }
-
+        private string Name = name;
+        private string UserId = userId;
+        private string Email = email;
+        private string BorrowedBooks = borrowedBooks;
     }
 }
 
