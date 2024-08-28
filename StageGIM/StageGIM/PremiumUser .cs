322d85170488722df03feaff8cd6e05b441bdcea
Ruveydabal <ruveydabal024@gmail.com>
2024-08-28 15:13:49 +0200

    PremiumUser
    made a start in PremiumUser

---
+++ StageGIM/StageGIM/PremiumUser .cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LibraryManagementSystem.clases;
+
+namespace LibraryManagementSystem.classes
+{
+    internal class PremiumUser(string name, string userId, string email, string borrowedBooks, string membershipLevel) : User(name, userId, email, borrowedBooks)
+    {
+        public string MembershipLevel { get; } = membershipLevel;
+    }
+}
