901b9c50674d14c7c434530a444dd1d1bc818d75
Ruveydabal <ruveydabal024@gmail.com>
2024-08-29 13:22:19 +0200

    PremiumUser
    Added namespace and class PremiumUser. it is far from done

--- StageGIM/StageGIM/PremiumUser .cs
+++ StageGIM/StageGIM/PremiumUser .cs
@@ -1,14 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using LibraryManagementSystem.clases;
 
-namespace LibraryManagementSystem.classes
+
+namespace LibraryManagementSystem.clases
 {
-    internal class PremiumUser(string name, string userId, string email, string borrowedBooks, string membershipLevel) : User(name, userId, email, borrowedBooks)
+    public class PremiumUser(string name, string userId, string email, string borrowedBooks, string membershipLevel, Library library) : User(name, userId, email, borrowedBooks)
     {
         public string MembershipLevel { get; } = membershipLevel;
+        public Library Library { get; }
+
+        public override void RequestBorrowBook()
+        {
+
+            base.RequestBorrowBook();
+        }
     }
 }
+
+            
+
+
