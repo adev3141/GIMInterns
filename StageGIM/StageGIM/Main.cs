f0a61a1d3d72b4ff29dce0ce89306b876d142b1c
Ruveydabal <ruveydabal024@gmail.com>
2024-08-30 08:46:52 +0200

    Main
    Testing of the methods are working like I inteded them to

--- StageGIM/StageGIM/Main.cs
+++ StageGIM/StageGIM/Main.cs
@@ -9,17 +9,15 @@ namespace LibraryManagementSystem.classes
         static void Main(string[] args)
         {
             var library = new Library();
-         //   var user = new User("Ruvyeda", "2", "ruveyda@ruveyda.nl", "0");
-            var pruser = new PremiumUser("Ruveyda", "2", "ruveyda@ruveyda.nl", "0", "1",library);
+            var user = new User("Ruveyda", "2", "ruveyda@ruveyda.nl", "0");
+          //  var pruser = new PremiumUser("Ruveyda", "2", "ruveyda@ruveyda.nl", "0", "1",library);
             
-
+            //testing if the methods work
             library.AddBook();
             library.AddBook();
-            pruser.RequestBorrowBook();
-           //user.RequestBorrowBook();
-       //  library.BorrowBook();
-         //   library.ReturnBook();
-          //  library.RemoveBook();
+            library.BorrowBook();
+            library.ReturnBook();
+            library.RemoveBook();
 
         }
     }
