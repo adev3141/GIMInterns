3bd1b4708bf714accead53119e2447a16a44e1e0
Ruveydabal <ruveydabal024@gmail.com>
2024-08-30 15:00:41 +0200

    User
    Added a method

--- StageGIM/StageGIM/User.cs
+++ StageGIM/StageGIM/User.cs
@@ -6,6 +6,7 @@ namespace LibraryManagementSystem.clases
     public class User(string name, string userId, string email, string borrowedBooks): Library
     {
         // Private fields to store the properties of the user
+        //I'm not using them for now :(
         private string Name = name;
         private string UserId = userId;
         private string Email = email;
@@ -15,7 +16,14 @@ namespace LibraryManagementSystem.clases
      
         public virtual void RequestBorrowBook(Library RequestBook)//now has all info from Library
         {
-            RequestBook.BorrowBook();    
+
+            RequestBook.BorrowBook();
+
+        }
+
+        public virtual void RequestAddBook(Library RequestAdd)
+        {//not sure if I will need to use this
+            RequestAdd.AddBook();
         }
     }
 }
