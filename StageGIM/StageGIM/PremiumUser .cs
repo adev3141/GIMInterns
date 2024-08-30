429852de08d0eb9ce498b6fedb835c14d5762293
Ruveydabal <ruveydabal024@gmail.com>
2024-08-30 11:32:33 +0200

    PremiumUser
    RequestBorrowBook works now

--- StageGIM/StageGIM/PremiumUser .cs
+++ StageGIM/StageGIM/PremiumUser .cs
@@ -14,11 +14,13 @@ namespace LibraryManagementSystem.clases
         public string MembershipLevel { get; } = membershipLevel;
         public Library Library { get; } = library;
 
-    //    public override void RequestBorrowBook()
-   //     {//does not work beacause BookToBorrow = null in Library - BorrowBook()
+    public override void RequestBorrowBook(Library Library)
+        {//does not work beacause BookToBorrow = null in Library - BorrowBook() goes wrong in User
 
-     //       base.RequestBorrowBook();
-     //   }
+          base.RequestBorrowBook(Library);
+        }
+
+   
     }
 }
 
