1b8017e6c7eb8cfd15483d1fb165f1918ee0c7d8
Ruveydabal <ruveydabal024@gmail.com>
2024-09-02 12:38:17 +0200

    Student Record System
    Started on Student Record Sytem exercise

---
+++ StageGIM/StageGIM/Student Record Management System/Student.cs
@@ -0,0 +1,23 @@
+﻿using LibraryManagementSystem;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentRecordManagementSystem
+{
+    internal class Student
+    {
+        public string? StudentID { get; set; }
+        public string? Name { get; set; }
+        public string? Course { get; set; }
+
+        public List<int> Grade = new List<int>();
+
+        public List<Student> StudentList = new List<Student>();
+
+
+    }
+
+}
