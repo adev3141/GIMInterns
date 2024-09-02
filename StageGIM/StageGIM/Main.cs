89a43a114df94d61c2550bd8bdd95b048841e9b5
Ruveydabal <ruveydabal024@gmail.com>
2024-09-02 10:07:56 +0200

    Main
    Tested a method that I made

--- StageGIM/StageGIM/Main.cs
+++ StageGIM/StageGIM/Main.cs
@@ -11,17 +11,20 @@ namespace LibraryManagementSystem.classes
             var library = new Library();
             var user = new User("Ruveyda", "2", "ruveyda@ruveyda.nl", "0");
            var pruser = new PremiumUser("Ruveyda", "2", "ruveyda@ruveyda.nl", "0", "1",library);
-            
+
             //testing if the methods work
+
+            
             library.AddBook();
             library.AddBook();
+            library.FindBook();
             pruser.RequestBorrowBook(library);
          //   user.BorrowBook();
          //  user.RequestBorrowBook(library);
          //    library.BorrowBook();
          //library.ReturnBook();
          //library.RemoveBook();
-
+        
         }
     }
 }
