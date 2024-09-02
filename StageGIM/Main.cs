b8af878b71e428a790df916e3d8f20c61bd08bb9
Ruveydabal <ruveydabal024@gmail.com>
2024-09-02 15:31:32 +0200

    Student management system
    Changed added AddStudents in Student

---
+++ StageGIM/Main.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentRecordManagementSystem
+{
+    class MainWindow
+    {
+        static void Main(string[] args)
+        {
+            Student kid = new Student();
+
+            kid.AddStudents();
+        }
+    }
+}
---
+++ StageGIM/Student Management/Student Management/Student.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentRecordManagementSystem
+{
+    internal class Student
+    {
+        public string? StudentID { get; set; }
+        public string? Name { get; set; }
+        public string? Course { get; set; }
+
+        public List<int> Grade = new List<int>();
+
+        public List<Student> StudentList = new List<Student>();
+        //made a dictionary 
+        public Dictionary<string, string> FindStudent = new Dictionary<string, string>();
+
+
+        public void AddStudents()
+        {     
+
+            Console.WriteLine("Adding the following students: ");
+            //Adds number as StudentID(entry key) and name as Name(enry value)
+            FindStudent.Add("1", "Alice Johnson");
+            FindStudent.Add("2", "Bob Smith");
+            FindStudent.Add("3", "Charlie Brown");
+
+            //Display the dictionary contents
+            Console.WriteLine("Current Students in the Directory:");
+            foreach (var entry in FindStudent)
+            {
+                Console.WriteLine($"StudentID: {entry.Key}, Name: {entry.Value}");
+            }
+
+            
+        }
+    }
+
+  
+
+}
