12544bc51d808a538a3c90babf89d4192d629768
Ruveydabal <ruveydabal024@gmail.com>
2024-09-02 15:30:12 +0200

    Student management
    Added them to a new file

---
+++ StageGIM/Student.cs
@@ -0,0 +1,32 @@
+﻿using LibraryManagementSystem;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentRecordManagementSystem
+{
+    internal class Student
+    {
+        public string? StudentID { get; set; }
+        public string? Name { get; set; }
+        public string? Course { get; set; }
+
+        public List<int> Grade = new List<int>();
+
+        public List<Student> StudentList = new List<Student>();
+
+        public Dictionary<string, string> FindStudent = new Dictionary<string, string>();
+
+    }
+
+    public void AddStudents()
+    {
+        Console.WriteLine("AddStudents");
+        FindStudent.Add("101", "Alice Johnson");
+        FindStudent.Add("102", "Bob Smith");
+        FindStudent.Add("103", "Charlie Brown");
+    }
+
+}
