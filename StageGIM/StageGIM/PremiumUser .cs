c142f1541a9c05f859b8572a884b7c97db2a5a76
Ruveydabal <ruveydabal024@gmail.com>
2024-09-02 09:02:01 +0200

    PemiumUser
    PremmiumUser can now borrow more than one book

--- StageGIM/StageGIM/PremiumUser .cs
+++ StageGIM/StageGIM/PremiumUser .cs
@@ -17,19 +17,29 @@ namespace LibraryManagementSystem.clases
         //Am to be able to use BorrowBook but need to be able to borrow more than one. 
 
         public override void RequestBorrowBook(Library Library)
-        {//need to change that it can borrow more then one book
-            Console.WriteLine("PremiumUser");
-            base.RequestBorrowBook(Library);
-          
+        {
+
+            Console.WriteLine("Give the number of books you want to borrow");//asks for amount of books to borrow and keeps it in variable
+            string AmountBookInput = Console.ReadLine();
+
+            //A loop that goes through iy=t by the amount put in by the user in AmountBookInput if the amount is a number above 0
+            if (int.TryParse(AmountBookInput, out int AmountBooks) && AmountBooks > 0)
+            {
+                for (int counter = 0; counter < AmountBooks; counter++)
+                {
+                    base.RequestBorrowBook(Library);
+                }
+            }
+            else
+            {//if the input is not a number or not above 0 it comes here
+                Console.WriteLine("Invalid number of books");
+            }
         }
 
-        public override void RequestAddBook(Library RequestAdd)//don't know if i need to use this for RequestBorrowBook
-        {
-            base.RequestAddBook(RequestAdd);
-        }
 
     }
 }
+
 
             
 
