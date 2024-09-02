a43d4a16f81628e21d2a0dbb1f3d496d4f1076f9
Ruveydabal <ruveydabal024@gmail.com>
2024-09-02 12:37:32 +0200

    folder
    Added them in the folder

---
+++ StageGIM/StageGIM/Library Management Sytem/Main.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Dynamic;
+using LibraryManagementSystem;
+
+namespace LibraryManagementSystem
+{
+    class MainWindow
+    {
+        static void Main(string[] args)
+        {
+            var library = new Library();
+            var user = new User("Ruveyda", "2", "ruveyda@ruveyda.nl", "0");
+           var pruser = new PremiumUser("Ruveyda", "2", "ruveyda@ruveyda.nl", "0", "1",library);
+
+            //testing if the methods work
+
+            
+            library.AddBook();
+            library.AddBook();
+            library.FindBook();
+            pruser.RequestBorrowBook(library);
+         //   user.BorrowBook();
+         //  user.RequestBorrowBook(library);
+         //    library.BorrowBook();
+         //library.ReturnBook();
+         //library.RemoveBook();
+        
+        }
+    }
+}
+
