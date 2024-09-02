0e2ee34e96b877af3cae22006da7a2215adffe4e
Ruveydabal <ruveydabal024@gmail.com>
2024-09-02 09:42:42 +0200

    IUser
    Made Interface User

---
+++ StageGIM/StageGIM/IUser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem.clases
+{
+    internal interface IUser
+    {//not sure if I did this right
+        void BorrowBook();
+
+        void RetunrBook();
+    }
+}
