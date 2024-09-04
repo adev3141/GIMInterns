c93d251eb254e8e4b87d2565592268eead2ab536
Ruveydabal <ruveydabal024@gmail.com>
2024-09-04 11:59:27 +0200

    Helpdesk
    Deleted things that i did not need

--- StageGIM/Student Management/Student Management/HelpDesk.cs
+++ StageGIM/Student Management/Student Management/HelpDesk.cs
@@ -11,7 +11,7 @@ namespace StudentRecordManagementSystem
         //queue for HelpDesk
         Queue<string> HelpDeskQueue = new Queue<string>();
         Student StudentHelpDesk { get; set; } = new Student();
-        
+
         public void AddToQueue()
         {//adds student to the Queue but does not display the Queue 
             Console.WriteLine("Please enter your Student ID to enter the queue:");
@@ -20,15 +20,9 @@ namespace StudentRecordManagementSystem
             // Adds the student ID to the queue
             this.HelpDeskQueue.Enqueue(studentID);
 
-            Console.WriteLine("You have been added to the queue.");
+            Console.WriteLine($"You have been added to the queue.");
 
         }
 
-        public void HelpDesk()
-        {
-            Console.WriteLine("Hello, this is the queue for the help desk...");
-            AddToQueue();
-         
-        }
     }
 }
