e1dff16ffbd6c0cce36c8bd3e7a025562893b377
Ruveydabal <ruveydabal024@gmail.com>
2024-09-06 08:40:21 +0200

    Added into folder

---
+++ StageGIM/Student Management/Student Management/Assignment-2/HelpDesk.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentRecordManagementSystem
+{
+    internal class HelpDeskSystem
+    {
+        //queue for HelpDesk
+        Queue<Student> HelpDeskQueue = new Queue<Student>();
+        Student StudentHelpDesk { get; set; } = new Student();
+
+        public void AddToQueue()//need to see if it adds in fifo manner
+        {//adds student to the Queue but does not display the Queue 
+
+            Console.WriteLine("Pleade enter your Name: ");
+            string StudentName = Console.ReadLine();
+
+            Console.WriteLine("Please enter your Student ID: ");
+            string studentID = Console.ReadLine();
+
+            //creates a new student object
+            Student student = new Student { Name = StudentName, StudentID = studentID };
+            this.HelpDeskQueue.Enqueue(student);
+        
+            Console.WriteLine($"You have been added to the queue.");
+
+            // Displays the contents of the queue.
+            Console.WriteLine("Current queue:");
+            foreach (var Student in this.HelpDeskQueue)
+            {
+                Console.WriteLine($"{Student.Name} (ID: {Student.StudentID})");
+            }
+        }
+
+    }
+}
---
+++ StageGIM/Student Management/Student Management/Assignment-2/Main.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StudentRecordManagementSystem;
+
+namespace StudentRecordManagementSystem
+{
+    class MainWindow
+    {
+        static void Main(string[] args)
+        {
+            Student kid = new Student();
+            HelpDeskSystem help = new HelpDeskSystem();
+            StudentManagement man = new StudentManagement();
+            bool Running = true;
+
+            while (Running) 
+            {
+                Console.WriteLine("Choose an option:");
+                Console.WriteLine("1. Add Student");
+                Console.WriteLine("2. Add Grade");
+                Console.WriteLine("3. Display Average Grade");
+                Console.WriteLine("4. Add student to HelpDesk Queue");
+                Console.WriteLine("5. Look Up Student");
+                Console.WriteLine("6. Remove Student");
+                Console.WriteLine("7. Update Student");
+                Console.WriteLine("8. Find and Display Student");
+                Console.WriteLine("9. Exit");
+
+                string Input =Console.ReadLine();
+
+                switch (Input)
+                {
+                    case "1":
+                        kid.StudentList.Add(man.AddStudent());
+                        break;
+                    case "2":
+                        man.AddGrade(kid.StudentList);
+                        break;
+                    case "3":
+                        man.AverageGrade(kid.StudentList);
+                        break;
+                    case "4":
+                        help.AddToQueue();
+                        break;
+                    case "5":
+                        man.LookUpStudent(kid.StudentID);
+                        break;
+                    case "6":
+                        man.RemoveStudent();
+                        break;
+                    case "7":
+                        man.UpdateStudent();
+                        break;
+                    case "8":
+                        kid.FindAndDisplayStudent(man);
+                        break;
+                    case "9":
+                        break;
+                }
+
+            }      
+        }
+    }
+}
