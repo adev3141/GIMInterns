15ade23dcc7d91d1dd1b19bb41910560fbe36fdf
Ruveydabal <ruveydabal024@gmail.com>
2024-09-12 13:59:35 +0200

    Created ClassRoom class
    Created ClassRoom class so I can put the student list there and not in the student class.

---
+++ StageGIM/Student Management/Student Management/Assignment-2/ClassRoom.cs
@@ -0,0 +1,15 @@
+﻿using StudentRecordManagementSystem;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student_Management.Assignment_2
+{
+    internal class ClassRoom
+    {
+        Student Students = new Student();
+        public List<Student> StudentList = new List<Student>();
+    }
+}
