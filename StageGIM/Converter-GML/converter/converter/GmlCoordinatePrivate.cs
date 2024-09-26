bd75b968a8b3f0b6eaf7689d9f5a6f9f166c233b
Ruveydabal <ruveydabal024@gmail.com>
2024-09-26 12:21:20 +0200

    created GmlCoordinateprivate class
    it is a private class that is going to have duplicate code from the GmlCoordinate class

---
+++ StageGIM/Converter-GML/converter/converter/GmlCoordinatePrivate.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace MyConverterNamespace
+{
+    internal class GmlCoordinatePrivate
+    {
+        public class getFeatures
+        {
+        
+            private string BeginMethodCoordinates(XNamespace GmlNamespace, XDocument xDocument, string points)
+            {
+                StringBuilder Coordinates = new StringBuilder();
+                //have to make it so that you chose point or line? and then use it in class gmlCoordinate
+
+                return "";         
+            }
+        }
+   
+    }
+}
