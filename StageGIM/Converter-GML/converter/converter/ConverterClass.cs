8657f96a54f8922e156f64c58ac1f5c14f771ac8
Ruveydabal <ruveydabal024@gmail.com>
2024-09-26 12:19:49 +0200

    created ConvertClass
    class where it converts the files

---
+++ StageGIM/Converter-GML/converter/converter/ConverterClass.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace MyConverterNamespace
+{
+    internal class converterClass
+    {
+        public XDocument GetFromXmlFile(string Path)
+        {
+            Console.WriteLine("File name? (Including extension)");
+            string FileNameLoad = Console.ReadLine();
+            string LoadFile = Path + FileNameLoad;
+
+
+            return XDocument.Load(LoadFile);
+        }
+
+      
+    }
+
+
+
+}
+
