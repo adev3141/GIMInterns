4d42052ed39410d2df5beaf85c29d9681c9c19b3
Ruveydabal <ruveydabal024@gmail.com>
2024-09-26 12:22:59 +0200

    created Main class
    changed main to Main this is going to be where the program is going to run

---
+++ StageGIM/Converter-GML/converter/converter/Main.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using static MyConverterNamespace.converterClass;
+
+namespace MyConverterNamespace
+{
+    class MainWindow
+    {
+
+        static void Main(string[] args)
+        {
+            getFeatures getFeatures = new getFeatures();
+            converter Converter = new converter();
+            string FilePath = "C:\\Users\\ruvey\\OneDrive - ROC Nijmegen\\school\\Webapplicatie\\webapp2\\GIMInterns\\gml-files";  //this is hard coded later make it so it is userinput? it has the gml file to read
+            XNamespace GmlNameSpace = "http://www.opengis.net/gml/3.2"; //makes it so that it can have gml
+            XDocument XDocumets = null;  // has to have a different value!!!
+        }
+              public class getFeatures
+        {
+
+        }
+
+        public class converter
+        {
+
+        }
+
+    }
+}
+
